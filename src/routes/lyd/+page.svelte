24a3698493bc8603c97490c251132572456c9530
kembak <kennethmbakken@gmail.com>
2024-04-25 23:16:14 +0200

    lydmedia

--- src/routes/lyd/+page.svelte
+++ src/routes/lyd/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+    import sangHøy from "$lib/assets/IronMaidenAcesHigh.mp3";
+    import sangLav from "$lib/assets/IronMaidenAcesHigh-low.mp3";
+</script>
 <h2>Lær om lyd</h2>
-<p>Lorem ipsum dolor sit amet</p>
+<p>hører du forskjellen på den første og den andre?</p>
+<audio controls src={sangHøy}></audio>
+<audio controls src={sangLav}></audio>
